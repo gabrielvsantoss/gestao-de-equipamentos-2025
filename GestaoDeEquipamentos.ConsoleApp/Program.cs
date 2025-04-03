edf793b7448984db6e107034fb2f3b4c40797c9c
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-04-03 16:53:47 -0300

    Implementada TelaEquipamento e classe regra de negócio Equiapmento

--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            TelaEquipamento telaEquipamento = new TelaEquipamento();
+
+            while (true)
+            {
+                string opcaoEscolhida = telaEquipamento.ApresentarMenu();
+
+                switch (opcaoEscolhida)
+                {
+                    case "1":
+                        telaEquipamento.CadastrarEquipamento();
+                        break;
+
+                    case "2":
+                        telaEquipamento.EditarEquipamento();
+                        break;
+
+                    case "3":
+                        telaEquipamento.ExcluirEquipamento();
+                        break;
+
+                    case "4":
+                        telaEquipamento.VisualizarEquipamentos(true);
+                        break;
+
+                    default:
+                        Console.WriteLine("Saindo do programa...");
+                        break;
+                }
+
+                Console.ReadLine();
+            }
+            
         }
     }
 }
