1a86a644ba88389c8afe22c89a67d9abb7e7718b
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-05-08 16:00:30 -0300

    Implementada edição de fabricantes

--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -19,6 +19,10 @@ class Program
 
         app.MapGet("/fabricantes/cadastrar", ExibirFormularioCadastroFabricante);
         app.MapPost("/fabricantes/cadastrar", CadastrarFabricante);
+
+        app.MapGet("/fabricantes/editar/{id:int}", ExibirFormularioEdicaoFabricante);
+        app.MapPost("/fabricantes/editar/{id:int}", EditarFabricante);
+
         app.MapGet("/fabricantes/visualizar", VisualizarFabricantes);
 
         app.Run();
@@ -62,6 +66,55 @@ class Program
         return context.Response.WriteAsync(conteudoString);
     }
 
+    static Task ExibirFormularioEdicaoFabricante(HttpContext context)
+    {
+        ContextoDados contextoDados = new ContextoDados(true);
+        IRepositorioFabricante repositorioFabricante = new RepositorioFabricanteEmArquivo(contextoDados);
+
+        int id = Convert.ToInt32(context.GetRouteValue("id"));
+
+        Fabricante fabricanteSelecionado = repositorioFabricante.SelecionarRegistroPorId(id);
+
+        string conteudo = File.ReadAllText("ModuloFabricante/Html/Editar.html");
+
+        StringBuilder sb = new StringBuilder(conteudo);
+
+        sb.Replace("#id#", id.ToString());
+        sb.Replace("#nome#", fabricanteSelecionado.Nome);
+        sb.Replace("#email#", fabricanteSelecionado.Email);
+        sb.Replace("#telefone#", fabricanteSelecionado.Telefone);
+
+        string conteudoString = sb.ToString();
+
+        return context.Response.WriteAsync(conteudoString);
+    }
+
+    static Task EditarFabricante(HttpContext context)
+    {
+        int id = Convert.ToInt32(context.GetRouteValue("id"));
+
+        ContextoDados contextoDados = new ContextoDados(true);
+        IRepositorioFabricante repositorioFabricante = new RepositorioFabricanteEmArquivo(contextoDados);
+
+        string nome = context.Request.Form["nome"].ToString();
+        string email = context.Request.Form["email"].ToString();
+        string telefone = context.Request.Form["telefone"].ToString();
+
+        Fabricante fabricanteAtualizado = new Fabricante(nome, email, telefone);
+
+        repositorioFabricante.EditarRegistro(id, fabricanteAtualizado);
+
+        string conteudo = File.ReadAllText("Compartilhado/Html/Notificacao.html");
+
+        StringBuilder sb = new StringBuilder(conteudo);
+
+        sb.Replace("#mensagem#", $"O registro \"{fabricanteAtualizado.Nome}\" foi editado com sucesso!");
+
+        string conteudoString = sb.ToString();
+
+        return context.Response.WriteAsync(conteudoString);
+    }
+
     static Task VisualizarFabricantes(HttpContext context)
     {
         ContextoDados contextoDados = new ContextoDados(true);
@@ -75,7 +128,7 @@ class Program
 
         foreach (Fabricante f in fabricantes)
         {
-            string itemLista = $"<li>{f.ToString()}</li> #fabricante#";
+            string itemLista = $"<li>{f.ToString()} / <a href=\"/fabricantes/editar/{f.Id}\">Editar</a> / <a href=\"/fabricantes/excluir/{f.Id}\">Excluir</a> </li> #fabricante#";
 
             stringBuilder.Replace("#fabricante#", itemLista);
         }
