45ab136638b18a9cd55ae180460aefe920a0e311
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-05-08 15:13:20 -0300

    Implementada feature Cadastrar Fabricante

--- GestaoDeEquipamentos.ConsoleApp/Program.cs
+++ GestaoDeEquipamentos.ConsoleApp/Program.cs
@@ -14,11 +14,52 @@ class Program
 
         WebApplication app = builder.Build();
 
+        // mapeamento de rotas
         app.MapGet("/", PaginaInicial);
 
+        app.MapGet("/fabricantes/cadastrar", ExibirFormularioCadastroFabricante);
+        app.MapPost("/fabricantes/cadastrar", CadastrarFabricante);
         app.MapGet("/fabricantes/visualizar", VisualizarFabricantes);
 
         app.Run();
+    }
+
+    static Task PaginaInicial(HttpContext context)
+    {
+        string conteudo = File.ReadAllText("Compartilhado/Html/PaginaInicial.html");
+
+        return context.Response.WriteAsync(conteudo);
+    }
+
+    static Task ExibirFormularioCadastroFabricante(HttpContext context)
+    {
+        string conteudo = File.ReadAllText("ModuloFabricante/Html/Cadastrar.html");
+
+        return context.Response.WriteAsync(conteudo);
+    }
+
+    static Task CadastrarFabricante(HttpContext context)
+    {
+        ContextoDados contextoDados = new ContextoDados(true);
+        IRepositorioFabricante repositorioFabricante = new RepositorioFabricanteEmArquivo(contextoDados);
+
+        string nome = context.Request.Form["nome"].ToString();
+        string email = context.Request.Form["email"].ToString();
+        string telefone = context.Request.Form["telefone"].ToString();
+
+        Fabricante novoFabricante = new Fabricante(nome, email, telefone);
+
+        repositorioFabricante.CadastrarRegistro(novoFabricante);
+
+        string conteudo = File.ReadAllText("Compartilhado/Html/Notificacao.html");
+
+        StringBuilder sb = new StringBuilder(conteudo);
+
+        sb.Replace("#mensagem#", $"O registro \"{novoFabricante.Nome}\" foi cadastrado com sucesso!");
+
+        string conteudoString = sb.ToString();
+
+        return context.Response.WriteAsync(conteudoString);
     }
 
     static Task VisualizarFabricantes(HttpContext context)
@@ -30,7 +71,9 @@ class Program
 
         StringBuilder stringBuilder = new StringBuilder(conteudo);
 
-        foreach (Fabricante f in repositorioFabricante.SelecionarRegistros())
+        List<Fabricante> fabricantes = repositorioFabricante.SelecionarRegistros();
+
+        foreach (Fabricante f in fabricantes)
         {
             string itemLista = $"<li>{f.ToString()}</li> #fabricante#";
 
@@ -42,12 +85,5 @@ class Program
         string conteudoString = stringBuilder.ToString();
 
         return context.Response.WriteAsync(conteudoString);
-    }
-
-    static Task PaginaInicial(HttpContext context)
-    {
-        string conteudo = File.ReadAllText("Html/PaginaInicial.html");
-
-        return context.Response.WriteAsync(conteudo);
     }
 }
